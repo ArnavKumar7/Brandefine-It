8d83cd187fa87f1548cdf250c94d2c9db7bbf583
ArnavKumar7 <arnav4776@gmail.com>
2023-04-18 00:26:41 +0530

    Wip: file renaming

--- app/api/users.js
+++ app/api/users.js
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
 
 export async function getStoredUsers() {
-  const rawFileCOntent = await fs.readFile("./users.json", {
+  const rawFileCOntent = await fs.readFile("./public/users.json", {
     encoding: "utf-8",
   });
   const data = JSON.parse(rawFileCOntent);
@@ -11,7 +11,7 @@ export async function getStoredUsers() {
 
 export function storeUsers(registrations) {
   return fs.writeFile(
-    "./users.json",
+    "./public/users.json",
     JSON.stringify({ registrations: registrations || [] })
   );
 }
